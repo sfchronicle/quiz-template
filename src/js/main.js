70f4fd8b064bfa32352783e0735018e8fba86f2e
emma <emmaruthoneill@gmail.com>
2016-12-08 14:47:15 -0800

    have answers working

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 require("./lib/social"); //Do not delete
+var closest = require("./lib/closest");
 
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
 
+var N = questionsData.length;
+var Nlist = Array.apply(null, {length: N}).map(Number.call, Number);
+Nlist.forEach(function(nn){
+  document.getElementById("answer-box-"+nn).addEventListener("click", function(e) {
+    var item = closest(e.target, (".answer"));
+    if (!item) return;
+    console.log(item);
+    console.log(item.classList);
+    console.log("#result"+nn);
+    if (hasClass(item,"x")) {
+      console.log("YES!");
+      document.querySelector("#result"+nn).innerHTML = "You got it!";
+    } else {
+      document.querySelector("#result"+nn).innerHTML = "Try again!";
+    }
+  });
+});
